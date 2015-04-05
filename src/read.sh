34fb16029d1dc0f82f20c63c76f8910f3075c974
ilix <me@ilix.se>
2015-04-05 11:45:48 +0200

    Cleaned up read.sh

--- src/read.sh
+++ src/read.sh
@@ -6,12 +6,7 @@ if [ ! -d $WORKDIR/data/ ]; then
   mkdir -p $WORKDIR/data/
 fi
 
-# Log data.
-log=$WORKDIR/log
-sudo pywws-hourly.py -v $HOMEDIR/data >$log 2>&1
-LOGDATA=`cat $log`
-
+sudo pywws-hourly.py -v $WORKDIR/data > $WORKDIR/read.log 2>&1
 sudo python -m pywws.Template $WORKDIR/data/ $WORKDIR/json.template $WORKDIR/latest.json
-JSONDATA=`cat $WORKDIR/latest.json`
 
-nodejs index.js
+node index.js
